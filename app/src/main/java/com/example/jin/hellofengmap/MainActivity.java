052e055436bbb88b9da2ed90864e08fe6e93fe2f
DongjinLiu <dongjin150153@outlook.com>
2017-07-26 18:40:16 +0800

    添加了一些功能

--- app/src/main/java/com/example/jin/hellofengmap/MainActivity.java
+++ app/src/main/java/com/example/jin/hellofengmap/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.jin.hellofengmap;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.fengmap.android.FMMapSDK;
 import com.fengmap.android.map.FMMap;
@@ -10,12 +15,23 @@ import com.fengmap.android.map.FMMapView;
 public class MainActivity extends AppCompatActivity {
 
     FMMap mFMMap;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         FMMapSDK.init(getApplication());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBar actionBar=getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
 
         FMMapView mapView = (FMMapView) findViewById(R.id.mapview);
         mFMMap = mapView.getFMMap();       //获取地图操作对象
@@ -30,5 +46,17 @@ public class MainActivity extends AppCompatActivity {
             mFMMap.onDestroy();
         }
         super.onBackPressed();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
