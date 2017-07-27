8428cf4fbcbc775042bc0c5d47f96224be03cf9c
DongjinLiu <dongjin150153@outlook.com>
2017-07-27 12:35:55 +0800

    用Activity替代了DrawerLayout，解决了手势冲突的问题

--- app/src/main/java/com/example/jin/hellofengmap/MainActivity.java
+++ app/src/main/java/com/example/jin/hellofengmap/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.jin.hellofengmap;
 
 import android.content.Intent;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
