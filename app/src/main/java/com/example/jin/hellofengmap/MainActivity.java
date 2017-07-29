8c3e269eb16783e7fbd46b7745a4e281844dee9b
cowarder <1615482151@qq.com>
2017-07-29 20:34:32 +0800

    添加了点击地图的提示框（自定义控件，可以通过xml修改） 添加更换头像事件

--- app/src/main/java/com/example/jin/hellofengmap/MainActivity.java
+++ app/src/main/java/com/example/jin/hellofengmap/MainActivity.java
@@ -504,6 +504,7 @@ public class MainActivity extends AppCompatActivity implements OnFMMapInitListen
             return true;
         }
 
+
         @Override
         public boolean onLongPress(FMNode node) {
             return false;
